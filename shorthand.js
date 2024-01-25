d002f813e2f0d6ae0065ec5e35dbc65c2dbeb1c3
Idba1 <moniraislamself@gmail.com>
2024-01-26 02:17:49 +0600

    shorthand.js

---
+++ shorthand.js
@@ -0,0 +1,19 @@
+var price = 30;
+// price = price + 10;
+price += 10;
+console.log(price);
+
+
+
+var Telivision = 30000;
+// price = price + 10;
+Telivision -= 1000;
+console.log(Telivision);
+
+
+
+
+var orangePrice = 300;
+// price = price + 10;
+orangePrice *= 5;
+console.log(orangePrice);
