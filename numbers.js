aeebec644987947cffccb65a77701ce752781216
Idba1 <moniraislamself@gmail.com>
2024-01-26 01:09:45 +0600

    numbers

---
+++ numbers.js
@@ -0,0 +1,20 @@
+var orangePrice = "20";
+var applePrice = 15;
+var orangePrice = parseInt("20");
+console.log(orangePrice + applePrice)
+
+
+
+var orangePrice = "20.3333";
+var applePrice = 15;
+var orangePrice = parseFloat("20.3333");
+console.log(orangePrice + applePrice)
+
+
+
+
+var orangePrice = "13";
+var applePrice = 1.2;
+var orangePrice = parseFloat("0.3333");
+mainPrice = orangePrice / applePrice;
+console.log(mainPrice. toFixed(2))
