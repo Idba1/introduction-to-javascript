e2eac79f51315fe4bb925f7e736f4d55f42821ca
Idba1 <moniraislamself@gmail.com>
2024-01-26 02:26:05 +0600

    mixrdOp.js

---
+++ mixedOp.js
@@ -0,0 +1,18 @@
+var firstName = "Mobarak";
+var lastName = "Chowdhury";
+console.log(firstName + lastName);
+
+
+var num1 = "400";
+var num2 = 44;
+console.log(num1 + num2);
+console.log(num1 - num2);
+console.log(num1 * num2);
+console.log(num1 / num2);
+
+
+
+var num1 = 400;
+var num2 = 44;
+total = num1 / num2;
+console.log(total. toFixed(2));
