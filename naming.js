b666d0028ea7ab6fc7d2f62eb79f70d5f10a192c
Idba1 <moniraislamself@gmail.com>
2024-01-26 01:33:50 +0600

    opertion.js

--- naming.js
+++ naming.js
@@ -1,26 +1,26 @@
-//  1. No keyword in variable name 
-var false = 9;
-var var = "wrong";
+// //  1. No keyword in variable name 
+// var false = 9;
+// var var = "wrong";
 
-//  2. No space in variable name
-var is rich = "false";
-var isrich = "right"
+// //  2. No space in variable name
+// var is rich = "false";
+// var isrich = "right"
 
 
-//  3. No quote
-var "isrich" = "false";
+// //  3. No quote
+// var "isrich" = "false";
 
-//  4. can not starting with a number 
-var 99group = "false";
+// //  4. can not starting with a number 
+// var 99group = "false";
 
-//  5. Name is case sensetive
-address
-Address
-addRess
+// //  5. Name is case sensetive
+// address
+// Address
+// addRess
 
-//  6. how to write long variable name 
+// //  6. how to write long variable name 
 
-var my current address = "United State of Ashulia";
-var my_current_address = "United State of Ashulia"; //snake_case
-var myCurrentAddress = "United State of Ashulia";   //camelCase
-var MyCurrentAddress = "United State of Ashulia"; //PasecaleCase
+// var my current address = "United State of Ashulia";
+// var my_current_address = "United State of Ashulia"; //snake_case
+// var myCurrentAddress = "United State of Ashulia";   //camelCase
+// var MyCurrentAddress = "United State of Ashulia"; //PasecaleCase
---
+++ operation.js
@@ -0,0 +1,47 @@
+// var number1 = 24;
+// var number2 = 30;
+// total = number1 + number2;
+// console.log(total);
+
+
+
+
+// var number1 = 44;
+// var number2 = 30;
+// total = number1 - number2;
+// console.log(total);
+
+
+
+// var number1 = 4;
+// var number2 = 3;
+// total = number1 * number2;
+// console.log(total);
+
+
+
+// var number1 = 44;
+// var number2 = 4;
+// total = number1 / number2;
+// console.log(total);
+
+
+
+// var number1 = 47;
+// var number2 = 4;
+// total = number1 % number2;
+// console.log(total);
+
+
+
+var shirtPrice = 47;
+console.log(shirtPrice);
+
+shirtPrice = shirtPrice + 50;
+console.log(shirtPrice);
+
+discountPrice = shirtPrice - 20;
+console.log(discountPrice);
+
+
+
